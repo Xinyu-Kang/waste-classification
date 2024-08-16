31c69aaefd7f2506f06296a02c6628f2782e7f65
Xinyu Kang <xinyu_kang@outlook.com>
2024-08-16 13:37:04 +0800

    create empty class

--- code/src/strategy.py
+++ code/src/strategy.py
@@ -1,3 +1,10 @@
 class SelectionStrategy:
+
     def __init__(self) -> None:
+        pass
+
+    def select(self, segmentation_results, depth_results):
+        pass
+
+    def depth_score():
         pass
