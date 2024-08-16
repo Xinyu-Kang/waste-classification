a2172cde3d4059dc64bb02b5163d2f3c5de23483
Xinyu Kang <xinyu_kang@outlook.com>
2024-08-16 13:37:17 +0800

    create flask app

--- code/src/app.py
+++ code/src/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
+from flask_cors import CORS
 import cv2
 import numpy as np
 
-from segmentation import SegmentationModel # 分割模型，YOLO
+from segmentation import SegmentationModel 
 from depth import DepthModel 
 from strategy import SelectionStrategy
 
 app = Flask(__name__) 
+CORS(app)
+https_bp = Blueprint('https', __name__)
+http_bp = Blueprint('http', __name__)
 
 segmentation_model = SegmentationModel("yolo")
 depth_model = DepthModel("depth-anything")
@@ -22,7 +26,7 @@ def process_image():
 
     depth_results = depth_model.predict(image, segmentation_results)
 
-    image_point, label = strategy.select(depth_results)
+    image_point, label = strategy.select(segmentation_results, depth_results)
 
     
     return jsonify({"grab_point": image_point.tolist(), "label": label})
@@ -30,3 +34,8 @@ def process_image():
 
 def get_image(data):
     pass
+
+if __name__ == '__main__':
+    app.register_blueprint(http_bp)
+    app.register_blueprint(https_bp)
+    app.run(host="0.0.0.0", port=5001)
