5749d00ff51d4dd2cc78c10f97dcb7c10812a99f
hehailin <feiyuzi51@gmail.com>
2024-08-19 17:26:03 +0800

    ftp图片存储功能
    异步任务进行图片存储

--- code/src/app.py
+++ code/src/app.py
@@ -3,6 +3,11 @@ from flask_cors import CORS
 import cv2
 import numpy as np
 import yaml
+import queue
+import ftplib
+import asyncio
+import aiofiles
+from datetime import datetime
 
 from segmentation import SegmentationModel 
 from depth import DepthModel 
@@ -12,6 +17,18 @@ app = Flask(__name__)
 CORS(app)
 https_bp = Blueprint('https', __name__)
 http_bp = Blueprint('http', __name__)
+
+
+# 队列用来存放从摄像头捕获的图片的二进制数据
+image_queue = queue.Queue()
+
+# ftp配置
+# FTP配置
+ftp_server = "183.222.62.175"
+ftp_username = "hhl"
+ftp_password = "GGhuman001"
+remote_folder = "/RnD_CD/DATA_2024_08_19/"
+
 
 # 直接读取配置文件
 with open('./config/yoloconfig.yaml', 'r') as file:
@@ -25,6 +42,21 @@ segmentation_model = SegmentationModel(seg_model_name)
 depth_model_name = config_depth['model']['selected']
 depth_model = DepthModel(depth_model_name)
 
+async def upload_to_ftp(image_path, filename):
+    try:
+        async with aiofiles.open(image_path, 'rb') as file:
+            data = await file.read()
+
+        with ftplib.FTP(ftp_server) as ftp:
+            ftp.login(ftp_username, ftp_password)
+            ftp.cwd(remote_folder)
+            ftp.storbinary(f'STOR {filename}', data)
+
+        print(f"Successfully uploaded {filename} to FTP server.")
+    except Exception as e:
+        print(f"Failed to upload {filename} to FTP server: {e}")
+
+
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
@@ -32,7 +64,10 @@ def process_image():
     从request拿到现场图片,进行语义分割和深度预测,并使用策略选择抓取物体。
     :return:物体抓取点在图片上的坐标及物体的类别
     """
-    image = get_image(request.files['image'])
+    image, filename = get_image(request.files['image'])
+
+    # 异步上传图像到FTP服务器
+    asyncio.create_task(upload_to_ftp(filename, filename))
 
     segmentation_results = segmentation_model.predict(image)
     label_names = segmentation_model.get_label_names()
@@ -43,7 +78,9 @@ def process_image():
     
     image_grab_point, label = strategy.select()
     print(f" point {image_grab_point}, label : {label}")
+
     return jsonify({'point': image_grab_point.tolist(), 'label': label})
+
 
 
 def get_image(data):  
@@ -53,7 +90,14 @@ def get_image(data):
     # 解码图像
     image = cv2.imdecode(image_np, cv2.IMREAD_UNCHANGED)
     
-    return image
+    # 获取当前时间作为文件名
+    filename = datetime.now().strftime("%Y%m%d_%H%M%S") + ".png"
+    
+    # 保存图像到本地
+    # cv2.imwrite(filename, image)
+    
+    # 返回解码后的图像和文件名
+    return image, filename
 
 
 
