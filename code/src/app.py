9fdb102e705c081499bb757ef49d63f447c91da6
hehailin <feiyuzi51@gmail.com>
2024-08-21 14:49:10 +0800

    更新app
    监控页面更新

--- code/src/app.py
+++ code/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, Blueprint
+from flask import Flask, request, jsonify, Blueprint, make_response, render_template, send_from_directory
 from flask_cors import CORS
 import cv2
 import numpy as np
@@ -8,10 +8,17 @@ import ftplib
 import asyncio
 import aiofiles
 from datetime import datetime
+import os
+import concurrent.futures
 
+from draw import save_monitoring_image
 from segmentation import SegmentationModel 
 from depth import DepthModel 
 from strategy import SelectionStrategy
+
+
+
+
 
 app = Flask(__name__) 
 CORS(app)
@@ -28,6 +35,13 @@ ftp_server = "183.222.62.175"
 ftp_username = "hhl"
 ftp_password = "GGhuman001"
 remote_folder = "/RnD_CD/DATA_2024_08_19/"
+
+# 监控图片目录
+IMAGE_FOLDER = './monitoring'
+
+# 确保目录存在
+if not os.path.exists(IMAGE_FOLDER):
+    os.makedirs(IMAGE_FOLDER)
 
 
 # 直接读取配置文件
@@ -57,7 +71,6 @@ async def upload_to_ftp(image_path, filename):
         print(f"Failed to upload {filename} to FTP server: {e}")
 
 
-
 @app.route('/process_image', methods=['POST'])
 def process_image():
     """
@@ -65,6 +78,20 @@ def process_image():
     :return:物体抓取点在图片上的坐标及物体的类别
     """
     image, filename = get_image(request.files['image'])
+    
+    # 确保图像是三通道（RGB）
+    if image.shape[-1] == 4:  # 如果图像是四通道（RGBA）
+        image = image[:, :, :3]  # 去掉Alpha通道
+
+        # 将上下30像素设置为黑色
+    height, width, _ = image.shape
+
+    # 上部30像素涂黑
+    image[:30, :] = [0, 0, 0]  # 纯黑色 [B, G, R]，即[0, 0, 0]
+
+    # 下部30像素涂黑
+    image[-30:, :] = [0, 0, 0]  # 纯黑色
+
 
     # 异步上传图像到FTP服务器
     # asyncio.create_task(upload_to_ftp(filename, filename))
@@ -72,15 +99,51 @@ def process_image():
     segmentation_results = segmentation_model.predict(image)
     label_names = segmentation_model.get_label_names()
 
+
+
+    # 如果分割模型没有返回结果，返回204 No Content
+    if segmentation_results is None or not segmentation_results:
+        return make_response('', 204)
+
     depth_map = depth_model.predict(image)
 
+    # 归一化深度图，确保与第二段代码一致
+    depth_map = (depth_map - depth_map.min()) / (depth_map.max() - depth_map.min()) * 255.0
+    depth_map = depth_map.astype(np.uint8)
+
+
+    # 如果深度模型没有返回结果，返回204 No Content
+    if depth_map is None or not depth_map.any():
+        return make_response('', 204)
+
     strategy = SelectionStrategy(image, segmentation_results, depth_map, label_names)
-    
+
     image_grab_point, label, points = strategy.select()
-    print(f" point {image_grab_point}, label : {label}, object_img_pints: {points}")
+
+    # 如果抓取点、标签或points为空，返回204 No Content
+    if image_grab_point is None or label is None or points is None:
+        return make_response('', 204)
+
+
+    # 使用线程池来异步保存监控图像
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(save_monitoring_image, image, segmentation_results, strategy.candidates, filename,  './monitoring')
+
+   
 
     return jsonify({'point': image_grab_point, 'label': label, 'object_img_pints': points})
 
+
+@app.route('/monitor')
+def index():
+    num_images = int(request.args.get('num_images', 10))
+    images = sorted(os.listdir(IMAGE_FOLDER), key=lambda x: os.path.getmtime(os.path.join(IMAGE_FOLDER, x)), reverse=True)
+    images = images[:num_images]
+    return render_template('index.html', images=images)
+
+@app.route('/image/<filename>')
+def image(filename):
+    return send_from_directory(IMAGE_FOLDER, filename)
 
 
 def get_image(data):  
@@ -93,8 +156,6 @@ def get_image(data):
     # 获取当前时间作为文件名
     filename = datetime.now().strftime("%Y%m%d_%H%M%S") + ".png"
     
-    # 保存图像到本地
-    # cv2.imwrite(filename, image)
     
     # 返回解码后的图像和文件名
     return image, filename
@@ -102,6 +163,10 @@ def get_image(data):
 
 
 if __name__ == '__main__':
+    # 在项目根目录下创建一个 'monitoring' 文件夹
+    monitoring_dir = './monitoring'
+    if not os.path.exists(monitoring_dir):
+        os.makedirs(monitoring_dir)
     app.register_blueprint(http_bp)
     app.register_blueprint(https_bp)
     app.run(host='0.0.0.0', port=5001)
